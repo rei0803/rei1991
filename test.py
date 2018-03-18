7a303d6f9a5fe6367992a5364cd108c3e047bb5b
rei0803 <37494993+rei0803@users.noreply.github.com>
2018-03-18 19:52:44 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+import t2
+import string
+print(string.digits)
+
+
+return 9+90
+
+fruits={'banana':'芭蕉','orange':'蜜柑','kiwi':'キウイ'}
+
+print (fruits['banana'])
+print('banana' in fruits.keys())
+print (fruits.keys())
